d7246298580490821139e898cafac2c15f02c364
Andrew Rivett <veggiefrog@gmail.com>
2012-06-14 09:02:30 -0400

    Added filedepot modules.  Upload and management working, downloading public files works

--- private/parseModuleCode.php
+++ private/parseModuleCode.php
@@ -72,7 +72,7 @@ function ciniki_systemdocs_parseModuleCode($ciniki, $package, $module) {
 	//
 	// Check for methods
 	//
-	$subdirs = array('scripts', 'public', 'private', 'cron');
+	$subdirs = array('scripts', 'public', 'private', 'cron', 'web');
 	foreach($subdirs as $subdir) {
 		$path = $ciniki['config']['core']['root_dir'] . '/' . $package . '-api/' . $module . '/' . $subdir;
 		if( !is_dir($path) ) {
