f4e13dfc34428d8ced263468d973e93780e63ea1
Andrew Rivett <veggiefrog@gmail.com>
2016-09-10 10:22:48 -0400

    Added hooks/uiSettings for new business menu

---
+++ hooks/uiSettings.php
@@ -0,0 +1,42 @@
+<?php
+//
+// Description
+// -----------
+// This function will return a list of user interface settings for the module.
+//
+// Arguments
+// ---------
+// ciniki:
+// business_id:     The ID of the business to get systemdocs for.
+//
+// Returns
+// -------
+//
+function ciniki_systemdocs_hooks_uiSettings($ciniki, $business_id, $args) {
+
+    //
+    // Setup the default response
+    //
+    $rsp = array('stat'=>'ok', 'menu_items'=>array());
+
+    //
+    // Check permissions for what menu items should be available
+    //
+    if( isset($ciniki['business']['modules']['ciniki.systemdocs'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $menu_item = array(
+            'priority'=>2100,
+            'label'=>'System Documentation', 
+            'edit'=>array('app'=>'ciniki.systemdocs.main'),
+            );
+        $rsp['menu_items'][] = $menu_item;
+    } 
+
+    return $rsp;
+}
+?>
